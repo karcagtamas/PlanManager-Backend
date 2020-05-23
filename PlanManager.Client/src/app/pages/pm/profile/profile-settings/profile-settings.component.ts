b92e12933076b6ac7dc2420adb5dead6d91210d1
Karcag Tamas <karcagtamas@outlook.com>
2020-05-23 17:18:05 +0200

    Update plan manager client

--- PlanManager.Client/src/app/pages/pm/profile/profile-settings/profile-settings.component.ts
+++ PlanManager.Client/src/app/pages/pm/profile/profile-settings/profile-settings.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models';
 
 @Component({
   selector: 'pm-profile-settings',
   templateUrl: './profile-settings.component.html',
-  styleUrls: ['./profile-settings.component.scss']
+  styleUrls: ['./profile-settings.component.scss'],
 })
 export class ProfileSettingsComponent implements OnInit {
-
-  constructor() { }
+  public roleNumbers: string;
+  public user: User = {
+    id: '1',
+    userName: 'barni363hun',
+    email: 'barni.pbs@gmail.com',
+    secondaryEmail: 'barni363hun@gmail.com',
+    fullName: 'Princzes Barnabás ',
+    registrationDate: new Date(),
+    lastLogin: new Date(),
+    roles: ['Developer', 'User'],
+    phone: 202790962,
+  };
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.user.roles.length > 1) {
+      this.roleNumbers = 'levels';
+    } else {
+      this.roleNumbers = 'level';
+    }
   }
-
 }
