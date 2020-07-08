a55d89bf9b14e7b6bad1124ede18c06dd3c104c5
Karcag Tamas <karcagtamas@outlook.com>
2020-07-08 22:29:42 +0200

    #14 Add service

---
+++ ManagerAPI.Services/Services/Interfaces/ILoggerService.cs
@@ -0,0 +1,15 @@
+﻿using ManagerAPI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerAPI.Services.Services.Interfaces
+{
+    public interface ILoggerService
+    {
+        void LogError(Exception e);
+        void LogInformation(string action, User user);
+        string AddUserToMessage(string message, User user);
+        string CreateMessage(string message, string args);
+    }
+}
