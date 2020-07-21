cf62368e315f2eb0eb12c7c7ca20707d2065c3bc
Karcag Tamas <karcagtamas@outlook.com>
2020-07-21 22:09:41 +0200

    #22 Repository pattern

---
+++ ManagerAPI.Services/Services/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ManagerAPI.Services.Services.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        List<TEntity> GetAll();
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> predicate);
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> predicate, int? count);
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> predicate, int? count, int? skip);
+        TEntity Get(int id);
+        TEntity Get(string id);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ ManagerAPI.Services/Services/Repository.cs
@@ -0,0 +1,91 @@
+﻿using ManagerAPI.Services.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ManagerAPI.Services.Services
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        public Repository(DbContext context) 
+        {
+            this.Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            this.Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            this.Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public TEntity Get(int id)
+        {
+            return this.Context.Set<TEntity>().Find(id);
+        }
+
+        public TEntity Get(string id)
+        {
+            return this.Context.Set<TEntity>().Find(id);
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return this.Context.Set<TEntity>().ToList();
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this.GetList(predicate, null, null);
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> predicate, int? count)
+        {
+            return this.GetList(predicate, count, null);
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> predicate, int? count, int? skip)
+        {
+            var query = this.Context.Set<TEntity>().Where(predicate);
+
+            if (count != null)
+            {
+                query = query.Take((int)count);
+            }
+
+            if (skip != null)
+            {
+                query = query.Skip((int)skip);
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            this.Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            this.Context.Set<TEntity>().RemoveRange(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            this.Context.Set<TEntity>().Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            this.Context.Set<TEntity>().UpdateRange(entities);
+        }
+    }
+}
