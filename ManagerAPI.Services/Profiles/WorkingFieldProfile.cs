81ce6b58e7b8afee5fa1aa426384ecdca859e7bf
Karcag Tamas <karcagtamas@outlook.com>
2020-07-28 19:37:46 +0200

    #32 Inactive days to Work days

--- ManagerAPI.Services/Profiles/WorkingFieldProfile.cs
+++ ManagerAPI.Services/Profiles/WorkingFieldProfile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using ManagerAPI.Domain.Entities.WM;
@@ -20,15 +21,15 @@ namespace ManagerAPI.Services.Profiles
                 .ForMember(dest => dest.Fields, opt => opt.MapFrom(src => src))
                 .ForMember(dest => dest.HourSum, opt => opt.MapFrom(src => this.CalcSumHour(src)))
                 .ForMember(dest => dest.HourAvg, opt => opt.MapFrom(src => this.CalcAvgHour(src)))
-                .ForMember(dest => dest.ActiveDays, opt => opt.MapFrom(src => this.Days(src, true)))
-                .ForMember(dest => dest.InactiveDays, opt => opt.MapFrom(src => this.Days(src, false)))
+                .ForMember(dest => dest.ActiveDays, opt => opt.MapFrom(src => this.ActiveDays(src)))
+                .ForMember(dest => dest.WorkDays, opt => opt.MapFrom(src => this.WorkDays(src)))
                 .ForMember(dest => dest.Counts, opt => opt.MapFrom(src => this.CalcCounts(src)));
             CreateMap<List<WorkingField>, WorkingWeekStatDto>()
                 .ForMember(dest => dest.Fields, opt => opt.MapFrom(src => src))
                 .ForMember(dest => dest.HourSum, opt => opt.MapFrom(src => this.CalcSumHour(src)))
                 .ForMember(dest => dest.HourAvg, opt => opt.MapFrom(src => this.CalcAvgHour(src)))
-                .ForMember(dest => dest.ActiveDays, opt => opt.MapFrom(src => this.Days(src, true)))
-                .ForMember(dest => dest.InactiveDays, opt => opt.MapFrom(src => this.Days(src, false)))
+                .ForMember(dest => dest.ActiveDays, opt => opt.MapFrom(src => this.ActiveDays(src)))
+                .ForMember(dest => dest.WorkDays, opt => opt.MapFrom(src => this.WorkDays(src)))
                 .ForMember(dest => dest.Counts, opt => opt.MapFrom(src => this.CalcCounts(src)));
         }
 
@@ -39,12 +40,38 @@ namespace ManagerAPI.Services.Profiles
 
         private double CalcAvgHour(List<WorkingField> fields)
         {
-            return fields == null || !fields.Any() ? 0 : (double)CalcSumHour(fields) / this.Days(fields, true);
+            return fields == null || !fields.Any() ? 0 : (double)CalcSumHour(fields) / this.ActiveDays(fields);
         }
 
-        private int Days(List<WorkingField> fields, bool? active) 
+        private int ActiveDays(List<WorkingField> fields) 
         {
-            return fields.GroupBy(x => x.WorkingDay).Select(x => x.Key).Count(x => active == null ? true : (bool)active ? x.Type.DayIsActive : !x.Type.DayIsActive);
+            return fields.GroupBy(x => x.WorkingDay).Select(x => x.Key).Count(x => x.Type.DayIsActive);
+        }
+
+        private int WorkDays(List<WorkingField> fields)
+        {
+            if (!fields.Any())
+            {
+                return 0;
+            }
+
+            int count = 0;
+            int year = fields[0].WorkingDay.Day.Year;
+            int month = fields[0].WorkingDay.Day.Month;
+            var date = new DateTime(year, month, 1);
+            var monthDays = DateTime.DaysInMonth(year, month);
+            
+            while (date.Day != monthDays)
+            {
+                if (date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    count++;
+                }
+
+                date = date.AddDays(1);
+            }
+
+            return count;
         }
 
         private List<WorkingDayTypeCountDto> CalcCounts(List<WorkingField> fields) 
--- ManagerAPI.Shared/DTOs/WM/WorkingMonthStatDto.cs
+++ ManagerAPI.Shared/DTOs/WM/WorkingMonthStatDto.cs
@@ -7,7 +7,7 @@ namespace ManagerAPI.Shared.DTOs.WM
         public decimal HourSum { get; set; }
         public double HourAvg { get; set; }
         public int ActiveDays { get; set; }
-        public int InactiveDays { get; set; }
+        public int WorkDays { get; set; }
         public List<WorkingDayTypeCountDto> Counts { get; set; }
         public List<WorkingFieldListDto> Fields { get; set; }
     }
