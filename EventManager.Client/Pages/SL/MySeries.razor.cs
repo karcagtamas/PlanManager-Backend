26f042bfe8bae1a5cb3ec113af6e3eb6cd3382f7
Karcag Tamas <karcagtamas@outlook.com>
2020-08-11 13:26:36 +0200

    #35 My series and selector dialog

---
+++ EventManager.Client/Pages/SL/MySeries.razor.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventManager.Client.Models;
+using EventManager.Client.Services.Interfaces;
+using EventManager.Client.Shared.Components.SL;
+using ManagerAPI.Shared.DTOs.MC;
+using ManagerAPI.Shared.Helpers;
+using Microsoft.AspNetCore.Components;
+
+namespace EventManager.Client.Pages.SL
+{
+    public partial class MySeries
+    {
+        [Inject] private ISeriesService SeriesService { get; set; }
+        [Inject] private NavigationManager Navigation { get; set; }
+        [Inject] private IModalService Modal { get; set; }
+
+        private List<MySeriesListDto> SeriesList { get; set; }
+        private bool IsLoading { get; set; }
+
+        private List<TableHeaderData> Header { get; set; } = new List<TableHeaderData>
+        {
+            new TableHeaderData
+                {PropertyName = "Title", DisplayName = "Title", IsSortable = false, Displaying = (e) => (string) e},
+            new TableHeaderData
+            {
+                PropertyName = "StartYear", DisplayName = "StartYear", IsSortable = false,
+                Displaying = (e) => WriteHelper.WriteNullableField((int?) e)
+            },
+            new TableHeaderData
+                {PropertyName = "Creator", DisplayName = "Creator", IsSortable = false, Displaying = (e) => (string) e}
+        };
+
+        private List<string> Footer { get; } = new List<string> {" ", " ", " ", " "};
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetMovies();
+        }
+
+        private async Task GetMovies()
+        {
+            IsLoading = true;
+            StateHasChanged();
+            SeriesList = await SeriesService.GetMyList();
+            IsLoading = false;
+            Footer[0] = SeriesList.Count().ToString();
+            StateHasChanged();
+        }
+
+        private void RedirectToData(MySeriesListDto series)
+        {
+            Navigation.NavigateTo($"/series/{series.Id}");
+        }
+
+        private void OpenEditMySeriesDialog()
+        {
+            var parameters = new ModalParameters();
+            parameters.Add("FormId", 1);
+
+            var options = new ModalOptions
+            {
+                ButtonOptions = {ConfirmButtonType = ConfirmButton.Save, ShowConfirmButton = true}
+            };
+
+            Modal.OnClose += EditMySeriesDialogClosed;
+
+            Modal.Show<SeriesSelectorDialog>("Edit My Series", parameters, options);
+        }
+
+        private async void EditMySeriesDialogClosed(ModalResult modalResult)
+        {
+            if (!modalResult.Cancelled && (bool) modalResult.Data) await GetMovies();
+
+            Modal.OnClose -= EditMySeriesDialogClosed;
+        }
+    }
+}
--- EventManager.Client/Pages/SL/MySeriesBase.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace EventManager.Client.Pages.SL
-{
-    public class MySeriesBase : ComponentBase
-    {
-        
-    }
-}
