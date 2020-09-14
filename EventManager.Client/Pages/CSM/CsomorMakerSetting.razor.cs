7309e50cdbebc6a6c534b9c6e14ca87b37941ec9
Karcag Tamas <karcagtamas@outlook.com>
2020-09-14 19:21:54 +0200

    #58 Add Csomor settings form

--- EventManager.Client/Pages/CSM/CsomorMakerSetting.razor.cs
+++ EventManager.Client/Pages/CSM/CsomorMakerSetting.razor.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using ManagerAPI.Shared.Models.CSM;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace EventManager.Client.Pages.CSM
 {
@@ -6,5 +10,26 @@ namespace EventManager.Client.Pages.CSM
     {
         [Parameter]
         public int? Id { get; set; }
+
+        private EditContext Context { get; set; }
+        private GeneratorSettingsModel Model { get; set; }
+        private List<PersonModel> Persons { get; set; }
+        private List<WorkModel> Works { get; set; }
+        private EditContext PersonContext { get; set; }
+        private PersonModel PersonModel { get; set; }
+        private EditContext WorkContext { get; set; }
+        private WorkModel WorkModel { get; set; }
+
+        protected override void OnInitialized()
+        {
+            this.Model = new GeneratorSettingsModel();
+            this.Context = new EditContext(this.Model);
+
+            this.WorkModel = new WorkModel();
+            this.PersonModel = new PersonModel();
+
+            this.WorkContext = new EditContext(this.WorkModel);
+            this.PersonContext = new EditContext(this.PersonModel);
+        }
     }
 }
--- ManagerAPI.Shared/Models/CSM/GeneratorSettingsModel.cs
+++ ManagerAPI.Shared/Models/CSM/GeneratorSettingsModel.cs
@@ -1,16 +1,32 @@
-﻿using ManagerAPI.Shared.DTOs.CSM;
+﻿using ManagerAPI.Shared.Annotations;
+using ManagerAPI.Shared.DTOs.CSM;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ManagerAPI.Shared.Models.CSM
 {
     public class GeneratorSettingsModel
     {
+        [Required]
+        [MaxLength(120)]
         public string Title { get; set; }
+
+        [Required]
         public DateTime Start { get; set; }
+
+        [Required]
         public DateTime Finish { get; set; }
+
+        [Required]
+        [MinNumber(1)]
+        [MaxNumber(8)]
         public int MaxWorkHour { get; set; } = 3;
+
+        [Required]
+        [MinNumber(1)]
+        [MaxNumber(4)]
         public int MinRestHour { get; set; } = 1;
         public List<PersonModel> Persons { get; set; }
         public List<Work> Works { get; set; }
