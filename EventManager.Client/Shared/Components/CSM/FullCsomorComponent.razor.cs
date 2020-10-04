98d6a9a10e02b508d2e4cedea81f7783d6d3965c
Karcag Tamas <karcagtamas@outlook.com>
2020-10-04 14:42:39 +0200

    #58 Different tables

--- EventManager.Client/Shared/Components/CSM/FullCsomorComponent.razor.cs
+++ EventManager.Client/Shared/Components/CSM/FullCsomorComponent.razor.cs
@@ -13,18 +13,65 @@ namespace EventManager.Client.Shared.Components.CSM
     {
         [Parameter]
         public GeneratorSettings Settings { get; set; }
-        public Dictionary<DateTime, List<Person>> Rows { get; set; }
-        public string HoveredPersonId { get; set; } = "-";
+        public Dictionary<DateTime, List<DisplayMember>> Rows { get; set; }
+        public string HoveredMemberId { get; set; } = "-";
+        public string Selected { get; set; }
+        public bool SelectedIsPerson { get; set; } = false;
 
         protected override void OnParametersSet()
         {
             this.CreateTable();
+            StateHasChanged();
         }
 
         private void CreateTable()
         {
-            this.Rows = new Dictionary<DateTime, List<Person>>();
+            this.Rows = new Dictionary<DateTime, List<DisplayMember>>();
+            if (this.SelectedIsPerson)
+            {
+                this.CreatePersonTable();
+            }
+            else
+            {
+                this.CreateWorkTable();
+            }
+        }
+
+        private void CreatePersonTable()
+        {
+            var persons = Settings.Persons.OrderBy(x => x.Name).ToList();
+
+            if (!string.IsNullOrEmpty(this.Selected))
+            {
+                persons = persons.Where(x => x.Id == this.Selected).ToList();
+            }
+
+            for (int i = 0; i < persons.Count; i++)
+            {
+                var tables = persons[i].Tables.OrderBy(x => x.Date).ToList();
+                for (int j = 0; j < tables.Count; j++)
+                {
+                    if (i == 0)
+                    {
+                        this.Rows.Add(tables[j].Date, new List<DisplayMember> { new DisplayMember(this.GetWork(tables[j].WorkId)) });
+                    }
+                    else
+                    {
+                        this.Rows[tables[j].Date].Add(new DisplayMember(this.GetWork(tables[j].WorkId)));
+                    }
+                }
+            }
+        }
+
+        private void CreateWorkTable()
+        {
             var works = Settings.Works.OrderBy(x => x.Name).ToList();
+
+            if (!string.IsNullOrEmpty(this.Selected))
+            {
+                works = works.Where(x => x.Id == this.Selected).ToList();
+            }
+
             for (int i = 0; i < works.Count; i++)
             {
                 var tables = works[i].Tables.OrderBy(x => x.Date).ToList();
@@ -32,11 +79,11 @@ namespace EventManager.Client.Shared.Components.CSM
                 {
                     if (i == 0)
                     {
-                        this.Rows.Add(tables[j].Date, new List<Person> { this.GetPerson(tables[j].PersonId) });
+                        this.Rows.Add(tables[j].Date, new List<DisplayMember> { new DisplayMember(this.GetPerson(tables[j].PersonId)) });
                     }
                     else
                     {
-                        this.Rows[tables[j].Date].Add(this.GetPerson(tables[j].PersonId));
+                        this.Rows[tables[j].Date].Add(new DisplayMember(this.GetPerson(tables[j].PersonId)));
                     }
                 }
             }
@@ -51,15 +98,82 @@ namespace EventManager.Client.Shared.Components.CSM
             return this.Settings.Persons.FirstOrDefault(x => x.Id == id);
         }
 
+        private Work GetWork(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+            return this.Settings.Works.FirstOrDefault(x => x.Id == id);
+        }
+
         private void Hover(string id)
         {
             if (string.IsNullOrEmpty(id))
             {
-                this.HoveredPersonId = "-";
+                this.HoveredMemberId = "-";
             }
             else
             {
-                this.HoveredPersonId = id;
+                this.HoveredMemberId = id;
+            }
+        }
+
+        private void SelectedWorkChanged(string val)
+        {
+            this.Selected = val;
+            this.CreateTable();
+            StateHasChanged();
+        }
+
+        private void SelectedPersonChanged(string val)
+        {
+            this.Selected = val;
+            this.CreateTable();
+            StateHasChanged();
+        }
+
+        private void TableTypeChanged(bool val)
+        {
+            this.SelectedIsPerson = val;
+            this.Selected = null;
+            this.CreateTable();
+            StateHasChanged();
+        }
+    }
+
+    public class DisplayMember
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public DisplayMember() { }
+
+        public DisplayMember(Person person)
+        {
+            if (person == null)
+            {
+                this.Id = "";
+                this.Name = "-";
+            }
+            else
+            {
+                this.Id = person.Id;
+                this.Name = person.Name;
+            }
+        }
+
+        public DisplayMember(Work work)
+        {
+            if (work == null)
+            {
+                this.Id = "";
+                this.Name = "-";
+            }
+            else
+            {
+                this.Id = work.Id;
+                this.Name = work.Name;
             }
         }
     }
