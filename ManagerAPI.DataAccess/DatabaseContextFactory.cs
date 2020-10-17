a543bcdd1fc7bbf8f646f4b5d341ad96efb2ed85
Karcag Tamas <karcagtamas@outlook.com>
2020-10-17 21:54:00 +0200

    Code cleanup

--- ManagerAPI.DataAccess/DatabaseContextFactory.cs
+++ ManagerAPI.DataAccess/DatabaseContextFactory.cs
@@ -1,7 +1,7 @@
-using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace ManagerAPI.DataAccess
 {
@@ -11,13 +11,13 @@ namespace ManagerAPI.DataAccess
     public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
     {
         public DatabaseContext CreateDbContext(string[] args)
-        { 
+        {
             IConfiguration config = new ConfigurationBuilder()
                 .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../ManagerAPI.Backend"))
                 .AddJsonFile("appsettings.json")
                 .Build();
-            
-            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>(); 
+
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
             optionsBuilder.UseSqlServer(config.GetConnectionString("ManagerDb"));
             return new DatabaseContext(optionsBuilder.Options);
         }
