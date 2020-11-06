c9ca6dc1e8eb3fcaeb250261100bee1365c3d2e1
Karcag Tamas <karcagtamas@outlook.com>
2020-11-06 21:25:27 +0100

    Update

---
+++ EventManager.Client/Shared/Navigators/MainNavigator.razor.cs
@@ -0,0 +1,35 @@
+﻿using EventManager.Client.Services;
+using EventManager.Client.Services.Interfaces;
+using ManagerAPI.Shared.Helpers;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManager.Client.Shared.Navigators
+{
+    public partial class MainNavigator
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Inject]
+        public IHelperService HelperService { get; set; }
+
+        private string GetWorkingManagerUri()
+        {
+            return $"/wm/{DateHelper.DateToNumberDayString(DateTime.Now)}";
+        }
+
+        private string GetWorkingMonthStatUri()
+        {
+            return $"/wm/month/{HelperService.CurrentYear()}/{HelperService.CurrentMonth()}";
+        }
+
+        private string GetWorkingWeekStatUri()
+        {
+            return $"/wm/week/{DateHelper.DateToNumberDayString(HelperService.CurrentWeek())}";
+        }
+    }
+}
